387c2fd4ecc790c70e4929ec97c6c9027b9d3be6
basicthinker@lab <basicthinker@lab>
2011-07-13 20:02:21 +0800

    Switch to non-blocking channel.write() and use thread-based model.

--- src/org/stanzax/quatrain/server/MrServer.java
+++ src/org/stanzax/quatrain/server/MrServer.java
@@ -144,9 +144,13 @@ public class MrServer {
                     0, dataLength);
             replyBuffer.putInt(0, dataLength - 4);
             
-            assert(channel.isBlocking());
+            assert(!channel.isBlocking());
             synchronized (channel) {
                 channel.write(replyBuffer);
+                while (replyBuffer.hasRemaining()) {
+                    Thread.yield();
+                    channel.write(replyBuffer);
+                }
             }
             
             if (Log.DEBUG) Log.action("Reply to .callID .length", callID, dataLength);
@@ -315,11 +319,6 @@ public class MrServer {
         public Handler(byte[] data, SocketChannel channel) {
             this.data = data;
             this.channel = channel;
-            try {
-                this.channel.configureBlocking(true);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
 
         @Override
