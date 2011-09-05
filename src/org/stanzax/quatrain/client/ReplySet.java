f1496ebddc95eea75d237a36cce4c44322c2a86e
basicthinker@lab <jinglei.ren@stanzax.com>
2011-09-06 02:17:37 +0800

    Add non-blocking isReady() to ReplySet and roll back to buffer/queue.

--- src/org/stanzax/quatrain/client/ReplySet.java
+++ src/org/stanzax/quatrain/client/ReplySet.java
@@ -23,7 +23,16 @@ public class ReplySet {
         this.returnType = type;
         this.timeout = timeout;
     }
-
+    
+    public void close() {
+        if (callID == 0) return;
+        waiting.remove(callID);
+    }
+    
+    public void finalize() throws Throwable {
+        super.finalize();
+        close();
+    }
     /**
      * Register for awaiting reply. 
      * Invoked before sending request to guarantee no reply omitted.
@@ -47,23 +56,8 @@ public class ReplySet {
         return !done;
     }
     
-    public boolean hasMore() {
-        if (replyQueue.size() > 0) return true;
-        else if (timedOut) return false;
-        else return !done;
-    }
-
-    public Object nextElement() {
-        if (!timedOut) {
-            try {
-                Object element = replyQueue.poll(timeout, TimeUnit.MILLISECONDS);
-                if (element == null) timedOut = true;
-                else if (!(element instanceof EOR)) return element;
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        return null;
+    public void putError(String errorMessage) {
+        errors.add(errorMessage);
     }
     
     public String errorMessage() {
@@ -75,12 +69,41 @@ public class ReplySet {
         return errorInfo.toString();
     }
     
+    /**
+     * Test whether new element it ready in non-blocking manner
+     * */
+    public synchronized boolean isReady() {
+        return buffer != null 
+            || (done && replyQueue.size() > 1)
+            || (!done && replyQueue.size() > 0);
+    }
+    
+    /**
+     * Tell whether nextElement() would safely return an element
+     * */
+    public boolean hasMore() {
+        if (isReady()) {
+            return true;
+        } else return tryNext();
+    }
+
+    public Object nextElement() {
+        if (buffer != null || tryNext()) {
+            try {
+                return buffer;
+            } finally {
+                buffer = null;
+            }
+        } else return null;
+    }
+    
     /** 
      * Input should only contain data entries
-     * @return true if actual data are put, false if EOF or error is encountered
+     * @return true if actual data are put, false otherwise
     */
     public boolean putData(DataInputStream dataIn) {
-        if (!timedOut) {
+        if (!timedOut) { // otherwise no additional elements would be retrieved,
+                         // so there would be no meaning to put in new data
             try {
                 while (dataIn.available() > 0) {
                     returnType.readFields(dataIn);
@@ -95,41 +118,47 @@ public class ReplySet {
         }
         return false;
     }
-
-    public void putError(String errorMessage) {
-        if (!timedOut) {
-            errors.add(errorMessage);
-        } else return;
-    }
     
-    public void putEnd() {
+    public synchronized void putEnd() {
         replyQueue.add(new EOR());
         done = true;
         if (Log.DEBUG) Log.action("[ReplySet] Call # reaches reply end.", callID);
     }
     
-    public void close() {
-        if (callID == 0) return;
-        waiting.remove(callID);
-    }
-    
-    public void finalize() throws Throwable {
-        super.finalize();
-        close();
-    }
-    
-    public static ReplySet get(int callID) {
-        return waiting.get(callID);
+    /**
+     * Try to retrieve an additional element and store it in {@link#buffer}.
+     * The {@link#buffer} would be overwritten, 
+     * so test whether it is null before invoking this method. <br>
+     * <p>This is the single point where {@link#replyQueue} is dequeued.
+     * */
+    private boolean tryNext() {
+        if (!timedOut) try {
+            Object next = replyQueue.poll(timeout, TimeUnit.MILLISECONDS);
+            if (next == null) timedOut = true;
+            else if (next instanceof EOR) next = null;
+            buffer = next;
+            return buffer != null;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
     
     private int callID = 0;
     private Writable returnType;
     private long timeout;
     private LinkedBlockingQueue<Object> replyQueue = new LinkedBlockingQueue<Object>();
+    private Object buffer = null;
     private Vector<String> errors = new Vector<String>();
-
-    private volatile boolean done = false; // whether the EOR is enqueued.
+    /** Indicates whether the EOR is enqueued. */
+    private volatile boolean done = false;
+    /** Indicates whether additional elements should be retrieved */
     private volatile boolean timedOut = false;
+    
+    public static ReplySet get(int callID) {
+        return waiting.get(callID);
+    }
+    
     private static ConcurrentHashMap<Integer, ReplySet> waiting = 
         new ConcurrentHashMap<Integer, ReplySet>();
 }
